809e938b8840a838bcf19cb0c5a958ccf4f15986
Dheeraj Sharma <greatnerve@gmail.com>
2025-01-12 04:11:05 +0530

    server added

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ app = FastAPI(
 
 app.include_router(user_router, prefix="/api/v1/user", tags=["User"])
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
